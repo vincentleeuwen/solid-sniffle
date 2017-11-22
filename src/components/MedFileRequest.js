11ef8b1344a01d4b0825a4ce0242df241300a662
Vincent van Leeuwen <vincentleeuwen@gmail.com>
2017-11-22 13:01:08 +0100

    add navbar

--- src/components/MedFileRequest.js
+++ src/components/MedFileRequest.js
@@ -11,29 +11,36 @@ import  {
   FormText,
   Label,
   Input,
+  Navbar,
+  NavbarBrand,
 } from 'reactstrap';
 
 export default class MedFileRequest extends React.Component {
   render() {
     return (
-      <Container>
-        <Row>
-          <Col xs={12} md={{ size: 6, offset: 3}}>
-            <Card>
-              <CardBody>
-                <pre>id: {this.props.match.params.id}</pre>
-                <FormGroup>
-                  <Input type='file' name='file' id='exampleFile' />
-                  <FormText color='muted'>
-                    Lorem ipsum dolorum.
-                  </FormText>
-                </FormGroup>
-                <Button color='primary'>Upload</Button>
-              </CardBody>
-            </Card>
-          </Col>
-        </Row>
-      </Container>
+      <div>
+        <Navbar dark color='primary' expand='md' >
+          <NavbarBrand href='/'>MyMedFile</NavbarBrand>
+        </Navbar>
+        <Container className='main'>
+          <Row>
+            <Col xs={12} md={{ size: 6, offset: 3}}>
+              <Card>
+                <CardBody>
+                  <pre>id: {this.props.match.params.id}</pre>
+                  <FormGroup>
+                    <Input type='file' name='file' id='exampleFile' />
+                    <FormText color='muted'>
+                      Lorem ipsum dolorum.
+                    </FormText>
+                  </FormGroup>
+                  <Button color='primary'>Upload</Button>
+                </CardBody>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
+      </div>
     );
   }
 }
