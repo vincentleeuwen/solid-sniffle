811204e404369c39e71e6f3da8402976182ae355
Vincent van Leeuwen <vincentleeuwen@gmail.com>
2017-11-22 12:49:34 +0100

    add upload form

--- src/components/MedFileRequest.js
+++ src/components/MedFileRequest.js
@@ -1,13 +1,39 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import  {
+  Button,
+  Card,
+  CardBody,
+  Container,
+  Row,
+  Col,
+  FormGroup,
+  FormText,
+  Label,
+  Input,
+} from 'reactstrap';
 
 export default class MedFileRequest extends React.Component {
   render() {
     return (
-      <div>
-        <p>Request med file:</p>
-        <p>{this.props.match.params.id}</p>
-      </div>
+      <Container>
+        <Row>
+          <Col xs={12} md={{ size: 6, offset: 3}}>
+            <Card>
+              <CardBody>
+                <pre>id: {this.props.match.params.id}</pre>
+                <FormGroup>
+                  <Input type='file' name='file' id='exampleFile' />
+                  <FormText color='muted'>
+                    Lorem ipsum dolorum.
+                  </FormText>
+                </FormGroup>
+                <Button color='primary'>Upload</Button>
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
     );
   }
 }
